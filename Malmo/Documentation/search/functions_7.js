6ad67c59466725ae54bedee0c87713ecc09c95f4
June Rademaker <J.Rademaker@live.nl>
2018-05-27 19:31:36 +0200

    Removed Malmo framework, left project files. Can clone this inside your Malmo framework I guess.

--- Malmo/Documentation/search/functions_7.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['iscolourmaprequested',['isColourMapRequested',['../classmalmo_1_1_mission_spec.html#ad8cbacf5f60600bfffd66a7e6aac5c53',1,'malmo::MissionSpec']]],
-  ['isdepthrequested',['isDepthRequested',['../classmalmo_1_1_mission_spec.html#a6dbddabb88a70be6edf2cfed55ff4292',1,'malmo::MissionSpec']]],
-  ['isluminancerequested',['isLuminanceRequested',['../classmalmo_1_1_mission_spec.html#acdaac97519b77a6d1ed98f0be037f99f',1,'malmo::MissionSpec']]],
-  ['isrecording',['isRecording',['../structmalmo_1_1_mission_record_spec.html#a5b5bbb7fb58a1722262f3f801ffa55b8',1,'malmo::MissionRecordSpec']]],
-  ['isvideorequested',['isVideoRequested',['../classmalmo_1_1_mission_spec.html#af80a194eae7d906b93d5aae293e4cb0e',1,'malmo::MissionSpec']]]
-];
