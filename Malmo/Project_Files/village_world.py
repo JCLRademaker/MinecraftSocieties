beff4b59ed534eef35cf81134974d6784bae856a
June Rademaker <J.Rademaker@live.nl>
2018-05-27 17:51:43 +0200

    Added the Malmo framework + the Project_Files folder. Put my work inside that folder. Your work should go in there too.

---
+++ Malmo/Project_Files/village_world.py
@@ -0,0 +1,107 @@
+# ------------------------------------------------------------------------------------------------
+# Spawns a world based on a seed. You spawn right next to a simple village surrounded by plains
+# and trees. Closeby is also water. There are probably some resources underground as well. I've 
+# seen pigs around the spawn place as well, could be good for farming food.
+# At " my_mission.setWorldSeed("1488218954") " you can set the seed.
+# TO KILL ALL VILLAGERS, OPEN CONSOLE WITH THE / KEY AND ENTER: kill @e[type=Villager]
+# ------------------------------------------------------------------------------------------------
+
+from __future__ import print_function
+from builtins import range
+import MalmoPython
+import os
+import sys
+import time
+
+if sys.version_info[0] == 2:
+    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
+else:
+    import functools
+    print = functools.partial(print, flush=True)
+
+missionXML='''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+            <Mission xmlns="http://ProjectMalmo.microsoft.com" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+            
+              <About>
+                <Summary>Hello world!</Summary>
+              </About>
+              
+              <ServerSection>
+				<ServerInitialConditions>
+					<Time>
+						<StartTime>1000</StartTime>
+						<AllowPassageOfTime>false</AllowPassageOfTime>
+					</Time>
+				</ServerInitialConditions>
+                <ServerHandlers>
+                  <DefaultWorldGenerator/>
+                  <ServerQuitFromTimeUp timeLimitMs="0"/>
+                  <ServerQuitWhenAnyAgentFinishes/>
+                </ServerHandlers>
+              </ServerSection>
+              
+              <AgentSection mode="Survival">
+                <Name>MalmoTutorialBot</Name>
+                <AgentStart/>
+                <AgentHandlers>
+                  <ObservationFromFullStats/>
+                  <ContinuousMovementCommands turnSpeedDegs="180"/>
+                </AgentHandlers>
+              </AgentSection>
+            </Mission>'''
+
+# Create default Malmo objects:
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse( sys.argv )
+except RuntimeError as e:
+    print('ERROR:',e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+	
+my_mission = MalmoPython.MissionSpec(missionXML, True)
+# my_mission.setWorldSeed("1488218954")
+my_mission.setWorldSeed("-770290065")
+my_mission_record = MalmoPython.MissionRecordSpec()
+
+# Attempt to start a mission:
+max_retries = 3
+for retry in range(max_retries):
+    try:
+        agent_host.startMission( my_mission, my_mission_record )
+        break
+    except RuntimeError as e:
+        if retry == max_retries - 1:
+            print("Error starting mission:",e)
+            exit(1)
+        else:
+            time.sleep(2)
+
+# Loop until mission starts:
+print("Waiting for the mission to start ", end=' ')
+world_state = agent_host.getWorldState()
+while not world_state.has_mission_begun:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission running ", end=' ')
+
+# Loop until mission ends:
+while world_state.is_mission_running:
+    print(".", end="")
+    time.sleep(0.1)
+    world_state = agent_host.getWorldState()
+    for error in world_state.errors:
+        print("Error:",error.text)
+
+print()
+print("Mission ended")
+# Mission has ended.
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MinecraftSocieties
 Games and Agents project.
+
+Our project files belong in the Project_Files folder.
