6ab83ddcac4e26f01c95fa4c6f7ca661ff684970
Lars Leijten <l.w.leijten@students.uu.nl>
2018-06-22 22:07:45 +0200

    Capital

---
+++ task/task.py
@@ -0,0 +1,10 @@
+
+class Task(object):
+    def __init__(self, agent):
+        self.agent = agent
+
+    def IsCompleted(self):
+        return False
+
+    def Execute(task, agent):
+        return False
