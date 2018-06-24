95409e90c8a45baa15275fd0da0b766efca4bf1e
Lars Leijten <l.w.leijten@students.uu.nl>
2018-06-24 21:04:59 +0200

    Chest ray test/fix

--- task/handIn.py
+++ task/handIn.py
@@ -8,12 +8,13 @@ class HandInTask(Task):
     def Execute(task, agent):
         chests = agent.block_list["chest"]
         chestLocation = (chests[0][0] + 0.5, 60, chests[0][1] + 0.5)
+        raydat = agent.data.get(u'LineOfSight',False)
         
-        if agent.MoveLookAtBlock(chestLocation):
+        if (raydat and raydat[u'type'] == "chest" and raydat["inRange"]) or agent.MoveLookAtBlock(chestLocation):
             # Adds items of a specified type to the chest
+            agent.AddItemsToChest(agent.data[u'inventory'], "chest", task.r)
             agent.SendCommand("move 0")
             agent.SendCommand("setPitch 0")
-            agent.AddItemsToChest(agent.data[u'inventory'], "chest", task.r)
             return True
         return False
 
